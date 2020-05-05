f19434cb3bc1d9dfdc25337d6226f6c156708147
Sukhachev Anton <mrsuh6@gmail.com>
2020-05-05 16:42:11 +0300

    reload timeout

--- script.js
+++ script.js
@@ -11,16 +11,21 @@
 (function() {
     'use strict';
 
-    let username = 'username';//заменить на настоящий логин
-    let password = 'password';//заменить на настоящий пароль
+    let username          = 'username';//заменить на настоящий логин
+    let password          = 'password';//заменить на настоящий пароль
+    let refreshTimeoutSec = 300;//период обновления страницы в секундах
 
     setInterval(function() {
-    if (document.querySelector('#exit')) {
-        return;
-    }
+        if (document.querySelector('#exit')) {
+            return;
+        }
 
-    document.querySelector('#username-element input').value = username;
-    document.querySelector('#password-element input').value = password;
-    document.querySelector('#submit-element input').click();
-}, 1000);
+        document.querySelector('#username-element input').value = username;
+        document.querySelector('#password-element input').value = password;
+        document.querySelector('#submit-element input').click();
+    }, 1000);
+
+    setInterval(function() {
+        document.location.reload();
+    }, refreshTimeoutSec * 1000);
 })();
